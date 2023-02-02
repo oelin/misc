a58941cd5b81313e0acd4e1f528c43cb28f2f8b9
oelin <42823429+oelin@users.noreply.github.com>
2023-02-02 00:04:39 +0000

    Create weird-crash-on-edge.js

---
+++ weird-crash-on-edge.js
@@ -0,0 +1,15 @@
+<canvas width=32 height=32 style='background:red;image-rendering:pixelated'>
+<script>
+const canv = document.querySelector('canvas')
+
+const ctx = canv.getContext('2d')
+const data = Array(32 * 32).fill(0)
+
+/*
+        for (let i=0; i < 32; i++) {
+                for (let j=0; j < 32; i++) {
+                        ctx.rect(i, j, 1, 1)
+                        ctx.fill()
+                }
+        }*/
+</script>
